d7119362b60b8c8ab1f277119812d35f87b1d72d
RickStead <ricksteadx@gmail.com>
2023-10-03 22:13:40 +0300

    init

--- OSGbot/Program.cs
+++ OSGbot/Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Interactions;
+using System.Reflection;
+
+namespace App {
+    class Program
+    {
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+        private CommandServiceConfig _commandConfig;
+        private ICommandContext _context;
+        private InteractionService _interactionService;
+        private ulong _guildId = 868119144288096277;
+
+        public static async Task Main(string[] args)
+        {
+            string? secret = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+
+            if (string.IsNullOrEmpty(secret))
+            {
+                Console.WriteLine("Secret not found. Make sure the environment variable is set.");
+                return;
+            }
+
+            var program = new Program();
+            await program.RunBotAsync(secret);
+        }
+
+        public async Task RunBotAsync(string Token)
+        {
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _commandConfig = new CommandServiceConfig
+            {
+                CaseSensitiveCommands = false,
+                DefaultRunMode = Discord.Commands.RunMode.Async
+            };
+            //_services = new InteractionService(_client.Rest); // You can inject services here if needed.
+
+            _client.Log += LogAsync;
+            _client.Ready += OnReadyAsync;
+
+            await _client.LoginAsync(TokenType.Bot, Token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log);
+        }
+
+        private async Task OnReadyAsync()
+        {
+            _interactionService = new InteractionService(_client.Rest);
+            await _interactionService.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            await _interactionService.RegisterCommandsToGuildAsync(_guildId);
+
+            _client.InteractionCreated += async (x) =>
+            {
+                var ctx = new SocketInteractionContext(_client, x);
+                await _interactionService.ExecuteCommandAsync(ctx, _services);
+            };
+            Console.WriteLine($"{_client.CurrentUser.Username} is connected!");
+        }
+    }
+}
---
+++ OSGbot/TestCommandModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+using Discord.Interactions;
+
+namespace App
+{
+    public class TestCommandModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("test-slash", "Says hello")]
+        public async Task HelloCommand()
+        {
+            await RespondAsync($"Hello, {Context.User.Mention}!");
+        }
+    }
+}
